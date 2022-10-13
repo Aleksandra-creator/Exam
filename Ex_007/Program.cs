e5459d88536557f34c69f4322c45c884ea05e7d7
Aleksandra Ledeneva <a.ledeneva@bk.ru>
2022-10-13 20:30:16 +0300

    Добавили ex_008, ex_007

---
+++ Ex_007/Program.cs
@@ -0,0 +1,23 @@
+﻿int sf1 = 1;
+int sf2 = 2;
+int sfd = 5;
+int dist = 100;
+int count = 0;
+
+while(dist < 10);
+{
+    bool friend = true;
+    if(friend = true);
+    {
+        int time = dist / (sf2 + sfd);
+        bool friend = true;
+
+    }
+    if(friend = false);
+    {
+        int time = dist / (sf1 + sfd);
+        bool friend = false;
+    }
+    Console.WriteLine(count);
+    count++;
+}
---
+++ Ex_008/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+System.Console.WriteLine ("Введите число:"); // Выводим подсказку на экран
+string inputString = Console.ReadLine(); // Получаем введенную строку 
+int value = Convert.ToInt32(inputString); // Преобразуем к целому типу
+
+int square = value * value; // Вычисляем квадрат
+System.Console.WriteLine(square); // Выводим значение в консоль
