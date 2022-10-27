abe325fd08b52cd0e8a7173b0d3b376fe1f9adc4
Aleksandra Ledeneva <a.ledeneva@bk.ru>
2022-10-27 23:41:04 +0300

    Добавила Ex_042

---
+++ Ex_042/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая генерирует последовательность случайных чисел из 10 элементов 
+// в диапазоне от 1 до 10, и подсчитывает, сколько сгенерировалось чисел больше 5.
+
+
+
+void GenNum (int[] args)
+{
+    int nums = new Random().Next(1, 10);
+    for (int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine (nums);
+    }
+}
+
+var nums = GenNum(10);
+if (nums >= 5)
+{
+    Console.WriteLine (nums);
+}
